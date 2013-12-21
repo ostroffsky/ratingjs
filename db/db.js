bb32e5723f88d87eb51188c25acf1703c9879ec0
Alexey Ostrovsky <ostroffsky@gmail.com>
2013-12-21 15:56:48 +0400

    DB module refactoring

--- db/db.js
+++ db/db.js
@@ -6,23 +6,28 @@
 var nStore = require('nstore');
 nStore = nStore.extend(require('nstore/query')());
 
+var dbAdress = {
+    teams: "db/teams.db",
+    events: "db/events.db"
+};
+
 /* TEAMS */
 exports.addTeam = function(name, cb) {
-    var db = nStore.new('db/teams.db', function () {
+    var db = nStore.new(dbAdress.teams, function () {
 
         db.save(null, {name: name}, cb);
     });
 };
 
 exports.removeTeam = function(id, cb) {
-    var db = nStore.new('db/teams.db', function () {
+    var db = nStore.new(dbAdress.teams, function () {
 
         db.remove(id, cb);
     });
 };
 
 exports.getTeams = function(cb) {
-    var db = nStore.new('db/teams.db', function () {
+    var db = nStore.new(dbAdress.teams, function () {
         db.all(cb);
     });
 };
@@ -33,28 +38,28 @@ exports.addEvent = function(data, cb, id) {
     // id - optional
     id = id || null;
 
-    var db = nStore.new('db/events.db', function () {
+    var db = nStore.new(dbAdress.events, function () {
 
         db.save(id, data, cb);
     });
 };
 
 exports.removeEvent = function(id, cb) {
-    var db = nStore.new('db/events.db', function () {
+    var db = nStore.new(dbAdress.events, function () {
 
         db.remove(id, cb);
     });
 };
 
 exports.getEvent = function(id, cb) {
-    var db = nStore.new('db/events.db', function () {
+    var db = nStore.new(dbAdress.events, function () {
 
         db.get(id, cb);
     });
 };
 
 exports.getEvents = function(cb) {
-    var db = nStore.new('db/events.db', function () {
+    var db = nStore.new(dbAdress.events, function () {
         db.all(cb);
     });
 };
